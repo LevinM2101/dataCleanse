66f9d3e53aca890f330c263dd91d535ddca51faf
Levin Moggert <levin@moggert.de>
2023-07-24 10:56:32 +0200

    Finished package

--- R/convert_to_numeric.R
+++ R/convert_to_numeric.R
@@ -2,7 +2,7 @@
 #'
 #' @param df A data frame.
 #' @param column_name The name of the column to convert.
-#' @return A data frame with the specified column converted to numeric.
+#' @return A data frame with the specified column converted to numeric. Non-numeric values are converted to NA.
 #' @examples
 #' data(mtcars)
 #' mtcars$mpg <- as.character(mtcars$mpg)
@@ -10,6 +10,5 @@
 #' @export
 convert_to_numeric <- function(df, column_name) {
   df[[column_name]] <- suppressWarnings(as.numeric(df[[column_name]]))
-  df[df[[column_name]] == "NaN", column_name] <- NA
   return(df)
 }
